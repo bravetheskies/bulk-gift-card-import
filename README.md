4dd63d0782017e4721576483eab90fc95ca7f0cd
Andrew Wise <andrew@andrewwise.co.uk>
2022-07-18 15:32:51 +0100

    Creation of gift cards from CSV functional

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 
 Bulk gift card import tool is command line Ruby tool for [Shopify](https://www.shopify.com/).
 
-:rotating_light: **This tool can only be used with [Shopify Plus](https://www.shopify.com/plus). You must create a custom app and request the gift card API endpoint to be enabled via [Shopify Plus Support](https://help.shopify.com/en/support).**
+:rotating_light: **This tool can only be used with [Shopify Plus](https://www.shopify.com/plus). You must create a custom app and [request the gift card API endpoint to be enabled](https://community.shopify.com/c/shopify-apis-and-sdks/gift-card-api-404-not-found-error/m-p/1397206/highlight/true#M74800) via [Shopify Plus Support](https://help.shopify.com/en/support).**
+
+## Setup
+
+The bulk gift card import tool is a simple Ruby command line tool. To install the package run:
+
+```
+bundle install
+```
+
+## Usage
+
+To bulk import gift cards create a CSV in the following format:
+
+| Code | Balance | Expires | Note |
+| --- | --- | --- | --- |
+| ABCD 1234 ABCD 1234 | 100.00 | 2025-01-01 | This is a note. |
+
+```
+ruby import.rb -f example.csv
+```
--- import.rb
+++ import.rb
@@ -4,27 +4,57 @@ require 'optparse'
 require 'csv'
 require 'fileutils'
 
+@options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby import.rb [options]"
+
+  opts.on("-v", "--verbose", "Show extra information") do
+    @options[:verbose] = true
+  end
+  opts.on("-f=FILE", "--file=FILE", "Input file") do |file|
+    @options[:input_file] = file
+  end
+end.parse!
+
+if @options[:input_file]
+  input = @options[:input_file]
+else
+  abort "Please select an input file."
+end
+
 puts "Contecting to store: #{ENV['SHOPIFY_DOMAIN']}"
 
-ShopifyAPI::Context.setup(
-  # These values are required but not actually used for private apps
-  api_key: "DUMMY_VALUE",
-  host_name: "DUMMY_VALUE",
-  scope: "DUMMY_VALUE",
+begin
+  ShopifyAPI::Context.setup(
+    # These values are required but not actually used for private apps
+    api_key: "DUMMY_VALUE",
+    host_name: "DUMMY_VALUE",
+    scope: "DUMMY_VALUE",
 
-  private_shop: ENV.fetch("SHOPIFY_DOMAIN"),
-  api_secret_key: ENV.fetch("TOKEN"), # Note that this is actually the admin token, not the api secret key
+    private_shop: ENV.fetch("SHOPIFY_DOMAIN"),
+    api_secret_key: ENV.fetch("TOKEN"),
 
-  session_storage: ShopifyAPI::Auth::FileSessionStorage.new, # This is only to be used for testing, more information in session docs
-  is_embedded: false, # Set to true if you are building an embedded app
-  is_private: true, # Set to true if you are building a private app
-  api_version: "2021-10" # The vesion of the API you would like to use
-)
+    session_storage: ShopifyAPI::Auth::FileSessionStorage.new,
+    is_embedded: false,
+    is_private: true,
+    api_version: "2022-07"
+  )
 
-session = ShopifyAPI::Utils::SessionUtils.load_current_session
+  session = ShopifyAPI::Utils::SessionUtils.load_current_session
+rescue => error
+  puts "Unable to connect to Shopify API: #{error.message}"
+end
 
-gift_card = ShopifyAPI::GiftCard.new(session: session)
-gift_card.note = "This is a note"
-gift_card.initial_value = 100.0
-gift_card.code = "ABCD EFGH IJKL MNOP"
-gift_card.save()
+if session and input
+  csv_in = CSV.parse(File.read(input), :headers => true)
+
+  csv_in.each do |row|
+    gift_card = ShopifyAPI::GiftCard.new(session: session)
+    gift_card.note = row["Note"]
+    gift_card.initial_value = row["Balance"]
+    gift_card.code = row["Code"]
+    gift_card.expires_on = row["Expires"]
+    gift_card.save()
+  end
+end
